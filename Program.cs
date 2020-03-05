9840c5855102561895c63bc18127b57ac795e2c9
stuartjshepherd <stuart@createstudios.com.au>
2020-03-06 08:43:21 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StaticClass
+{
+    public static class Helper
+    {
+        public static string FullName(string FirstName, string LastName)
+        {
+            return $"Full Name: {FirstName} {LastName}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("A class with static keyword that contains only static members is defined as static class. A static class cannot be instantiated.");
+            Console.WriteLine();
+
+            Console.Write(Helper.FullName("Stuart", "Shepherd"));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Static Class Example
+A class with static keyword that contains only static members is defined as static class. A static class cannot be instantiated.
+
+
+## Getting Started
+
+### Prerequisites
+
+[.NET Core 3.1 SDK or later](https://dotnet.microsoft.com/download/dotnet-core/3.1)
